7e309a927b32173610ffcf5f93f4e7eb03469ddb
miranda956 <musularonald17@gmail.com>
2021-11-08 00:08:41 +0300

    first commit

---
+++ frontend/admin/UI/src/components/Footers/AdminFooter.js
@@ -0,0 +1,12 @@
+
+import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      
+    </footer>
+  );
+};
+
+export default Footer;
